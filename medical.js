b4588cb66433067dc6ef19e3d4f2eac7510446be
student-dev143 <raniarimsha5@gmail.com>
2026-08-29 17:49:18 +0500

    Add files via upload

---
+++ medical.js
@@ -0,0 +1,45 @@
+let btn = document.querySelector("#nav-booking");
+let contact = document.querySelector(".book-container")
+let sideNav = document.querySelector(".side-nav");
+let menuBar = document.querySelector(".ri-menu-line")
+const certificateSection = document.querySelector("#certificates-credentials");
+let bookBtn = document.querySelector("#home-booking");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+
+            certificateSection.style.animation = "none";
+
+            void certificateSection.offsetWidth;
+
+            certificateSection.style.animation =
+                "certificateReveal 1s linear";
+        }
+    });
+}, {
+    threshold: 0.3
+});
+
+observer.observe(certificateSection);
+
+
+
+
+
+
+
+
+menuBar.addEventListener("click",()=>{
+   sideNav.classList.toggle("active")
+});
+
+
+
+btn.addEventListener("click",function () {
+  contact.classList.toggle("book")
+})
+// bookBtn.addEventListener("click",function () {
+//   contact.classList.toggle("book")
+// })
+
